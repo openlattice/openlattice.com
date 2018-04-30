fde7e50522ce6dd909dbcb27b75fafa853070471
Corwin Crownover <corwin.crownover@gmail.com>
2018-05-01 00:18:53 +0200

    Add remaining info tiles

--- src/containers/technology/Technology.js
+++ src/containers/technology/Technology.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import HomepageSection from '../../components/layout/HomepageSection';
+import StyledContainerHorizontal from '../../components/layout/StyledContainerHorizontal';
 import InfoTile from './components/InfoTile';
 import * as TechConsts from './Consts';
 import { StyledTitleMd, StyledBody } from '../../utils/Styles';
@@ -42,6 +43,11 @@ const StyledContentBody = StyledBody.extend`
   margin-bottom: 70px;
 `;
 
+const TilesRow = StyledContainerHorizontal.extend`
+  justify-content: space-between;
+  padding-bottom: 30px;
+`;
+
 const Technology = () => (
   <HomepageSection background='#ffffff' paddingBottom='190px'>
     <StyledContentBlock>
@@ -57,11 +63,35 @@ const Technology = () => (
           more tailored products to governmental workflows, described below. 
         `}
       </StyledContentBody>
+    </StyledContentBlock>
+    <TilesRow>
       <InfoTile
           title={TechConsts.title1}
           body={TechConsts.body1}
           image={TechConsts.image1} />
-    </StyledContentBlock>
+      <InfoTile
+          title={TechConsts.title2}
+          body={TechConsts.body2}
+          image={TechConsts.image2} />
+      <InfoTile
+          title={TechConsts.title3}
+          body={TechConsts.body3}
+          image={TechConsts.image3} />
+    </TilesRow>
+    <TilesRow>
+      <InfoTile
+          title={TechConsts.title4}
+          body={TechConsts.body4}
+          image={TechConsts.image4} />
+      <InfoTile
+          title={TechConsts.title5}
+          body={TechConsts.body5}
+          image={TechConsts.image5} />
+      <InfoTile
+          title={TechConsts.title6}
+          body={TechConsts.body6}
+          image={TechConsts.image6} />
+    </TilesRow>
   </HomepageSection>
 );
 
