3acf27774615859bfba933e5d1807d4e908c0663
Corwin Crownover <corwin.crownover@gmail.com>
2018-04-30 16:50:25 +0200

    Add placeholder StyledButton

---
+++ src/components/buttons/StyledButton.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.div`
+  background: #F1F1F1;
+  border-radius: 3px;
+  height: 30px;
+  min-width: 30px;
+`;
+
+export default StyledButton;
