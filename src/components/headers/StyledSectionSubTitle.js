2d79117023fbf2910e3e2f4832ffb1bc8d49548a
Corwin Crownover <corwin.crownover@gmail.com>
2018-05-14 17:43:48 +0200

    Center title and subtitles at small size

--- src/components/headers/StyledSectionSubTitle.js
+++ src/components/headers/StyledSectionSubTitle.js
@@ -6,6 +6,11 @@ const StyledSectionSubTitle = styled.h2`
   color: ${N2};
   font-size: 16px;
   font-weight: normal;
+  text-align: center;
+
+  @media only screen and (min-width: 1020px) {
+    text-align: left
+  }
 `;
 
 export default StyledSectionSubTitle;
--- src/components/headers/StyledSectionTitle.js
+++ src/components/headers/StyledSectionTitle.js
@@ -4,6 +4,11 @@ const StyledSectionTitle = styled.h1`
   font-family: 'Chivo', sans-serif;
   font-size: 32px;
   font-weight: normal;
+  text-align: center;
+
+  @media only screen and (min-width: 1020px) {
+    text-align: left
+  }
 `;
 
 export default StyledSectionTitle;
