6eddada93f7e4a773b5878cbec7bc86ef2ade56e
Corwin Crownover <corwin.crownover@gmail.com>
2018-06-11 12:08:27 +0200

    Set up content to be rendered based on tab

--- src/containers/howitworks/HowItWorksSection.js
+++ src/containers/howitworks/HowItWorksSection.js
@@ -53,46 +53,154 @@ const TabItem = styled.span`
 `;
 
 const TabDisplayContentWrapper = styled.div`
-  background: blue;
   display: flex;
-  height: 500px;
+  height: 602px;
+  width: 100%;
+`;
+
+const TabDisplayContentLeft01 = styled.div`
+  background: blue;
+  height: 100%;
+  min-width: 435px;
+`;
+
+const TabDisplayContentRight01 = styled.div`
+  background: pink;
+  height: 100%;
+  width: 100%;
+`;
+
+const TabDisplayContentLeft02 = styled.div`
+  background: red;
+  height: 100%;
+  min-width: 435px;
+`;
+
+const TabDisplayContentRight02 = styled.div`
+  background: pink;
+  height: 100%;
+  width: 100%;
+`;
+
+const TabDisplayContentLeft03 = styled.div`
+  background: yellow;
+  height: 100%;
+  min-width: 435px;
+`;
+
+const TabDisplayContentRight03 = styled.div`
+  background: pink;
+  height: 100%;
+  width: 100%;
+`;
+
+const TabDisplayContentLeft04 = styled.div`
+  background: green;
+  height: 100%;
+  min-width: 435px;
+`;
+
+const TabDisplayContentRight04 = styled.div`
+  background: pink;
+  height: 100%;
   width: 100%;
 `;
 
 
-const HowItWorksSection = () => (
+const render01 = () => (
   <Fragment>
-    <PageSection bgColor={N1}>
-      <Content>
-        <StyledSectionTitle>
-          How It Works
-        </StyledSectionTitle>
-        <StyledCroppedSubtitle>
-          {
-            `As a public benefit corporation, OpenLattice enables precision government by linking individual-level data 
-            across currently siloed criminal justice, healthcare, and social services information at little or no cost.`
-          }
-        </StyledCroppedSubtitle>
-        <TabDisplayWrapper>
-          <TabDisplayNavigation>
-            <TabItem>
-              01 Problem Definition
-            </TabItem>
-            <TabItem>
-              02 Data Sharing Agreements
-            </TabItem>
-            <TabItem>
-              03 Establishing Data Access
-            </TabItem>
-            <TabItem>
-              04 Specific Services
-            </TabItem>
-          </TabDisplayNavigation>
-          <TabDisplayContentWrapper />
-        </TabDisplayWrapper>
-      </Content>
-    </PageSection>
+    <TabDisplayContentLeft01 />
+    <TabDisplayContentRight01 />
   </Fragment>
 );
+
+const render02 = () => (
+  <Fragment>
+    <TabDisplayContentLeft02 />
+    <TabDisplayContentRight02 />
+  </Fragment>
+);
+
+const render03 = () => (
+  <Fragment>
+    <TabDisplayContentLeft03 />
+    <TabDisplayContentRight03 />
+  </Fragment>
+);
+
+const render04 = () => (
+  <Fragment>
+    <TabDisplayContentLeft04 />
+    <TabDisplayContentRight04 />
+  </Fragment>
+);
+
+const renderContent = (tab) => {
+  switch(tab) {
+    case 1:
+      return render01();
+    case 2:
+      return render02();
+    case 3:
+      return render03();
+    case 4:
+      return render04();
+    default:
+      return render01();
+  }
+}
+
+
+class HowItWorksSection extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tab: 1
+    };
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <PageSection bgColor={N1}>
+          <Content>
+            <StyledSectionTitle>
+              How It Works
+            </StyledSectionTitle>
+            <StyledCroppedSubtitle>
+              {
+                `As a public benefit corporation, OpenLattice enables precision government by linking individual-level data 
+                across currently siloed criminal justice, healthcare, and social services information at little or no cost.`
+              }
+            </StyledCroppedSubtitle>
+            <TabDisplayWrapper>
+              <TabDisplayNavigation>
+                <TabItem>
+                  01 Problem Definition
+                </TabItem>
+                <TabItem>
+                  02 Data Sharing Agreements
+                </TabItem>
+                <TabItem>
+                  03 Establishing Data Access
+                </TabItem>
+                <TabItem>
+                  04 Specific Services
+                </TabItem>
+              </TabDisplayNavigation>
+              <TabDisplayContentWrapper>
+                {
+                  renderContent(this.state.tab)
+                }
+              </TabDisplayContentWrapper>
+            </TabDisplayWrapper>
+          </Content>
+        </PageSection>
+      </Fragment>
+    );
+  }
+};
+  
 
 export default HowItWorksSection;
