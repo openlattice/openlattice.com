7e2aff5a97fa9816fb155036653100a849c21dd7
Corwin Crownover <corwin.crownover@gmail.com>
2018-06-15 15:03:05 +0100

    Add constants for labels

---
+++ src/containers/orgs/Constants.js
@@ -0,0 +1,13 @@
+export const DANE_COUNTY = {
+  X: '601px',
+  Y: '286px',
+  LOCATION: 'Dane County, WI',
+  POPULATION: '531,273 People'
+}
+
+export const FRANKLIN_COUNTY = {
+  X: '784px',
+  Y: '286px',
+  LOCATION: 'Franklin County, OH',
+  POPULATION: '1,264,518 People'
+}
