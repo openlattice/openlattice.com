165c16b0449ace04bffbe46b17441c6afa9ee67c
Giulia Campana <giuliaecampana@gmail.com>
2019-03-15 23:10:57 -0700

    Create individual product pages

---
+++ src/containers/products/PCMPage.js
@@ -0,0 +1,14 @@
+/*
+ * @flow
+ */
+import React from 'react';
+
+import ComingSoon from '../../components/placeholder/ComingSoon';
+
+const PCMPage = () => (
+  <>
+    <ComingSoon />
+  </>
+);
+
+export default PCMPage;
---
+++ src/containers/products/RideAlongPage.js
@@ -0,0 +1,14 @@
+/*
+ * @flow
+ */
+import React from 'react';
+
+import ComingSoon from '../../components/placeholder/ComingSoon';
+
+const RideAlongPage = () => (
+  <>
+    <ComingSoon />
+  </>
+);
+
+export default RideAlongPage;
