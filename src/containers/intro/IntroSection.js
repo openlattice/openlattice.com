f901e9050b2ce5268183d2da46d2d455e03ed4e8
Giulia <giuliaecampana@gmail.com>
2019-10-22 11:52:57 -0700

    Add menu to home page

--- src/containers/intro/IntroSection.js
+++ src/containers/intro/IntroSection.js
@@ -3,8 +3,8 @@
  */
 
 import React from 'react';
-
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo-white.png';
 
@@ -14,11 +14,38 @@ import StyledSectionSubTitle from '../../components/headers/StyledSectionSubTitl
 import StyledSectionTitle from '../../components/headers/StyledSectionTitle';
 import { N0, PK0 } from '../../core/style/Colors';
 import { WINDOW_EDGE_PADDING, MEDIA_QUERY_MD, MEDIA_QUERY_LG } from '../../core/style/Sizes';
+import * as Routes from '../../core/router/Routes';
+
+const helpLink = 'https://help.openlattice.com';
+const scheduleACallLink = 'https://calendly.com/openlattice/openlattice-web-request';
 
 const Header = styled.div`
   display: flex;
   flex: 1 0 auto;
+  justify-content: space-between;
   margin-top: ${WINDOW_EDGE_PADDING}px;
+`;
+
+const Menu = styled.div`
+  align-items: center;
+  display: flex;
+  font-size: 14px;
+  justify-content: space-between;
+`;
+
+const MenuInternalLink = styled(Link)`
+  color: ${N0};
+  margin-left: 30px;
+  text-decoration: none;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const MenuExternalLink = styled.a`
+  color: ${N0};
+  margin-left: 30px;
+  text-decoration: none;
 `;
 
 const Content = styled.div`
@@ -89,6 +116,10 @@ const IntroSection = () => (
   <PageSection bgComponent={BackgroundComponent}>
     <Header>
       <img src={logoImg} alt="OpenLattice Logo" height={50} />
+      <Menu>
+        <MenuInternalLink to={Routes.PRODUCTS}>Products</MenuInternalLink>
+        <MenuExternalLink href={helpLink}>Help</MenuExternalLink>
+      </Menu>
     </Header>
     <Content>
       <Title>
@@ -97,7 +128,7 @@ const IntroSection = () => (
       <SubTitle>
         Foundational tools to better serve all citizens
       </SubTitle>
-      <ScheduleLink href="https://calendly.com/openlattice/openlattice-web-request">
+      <ScheduleLink href={scheduleACallLink}>
         Schedule a call to learn more
       </ScheduleLink>
     </Content>
