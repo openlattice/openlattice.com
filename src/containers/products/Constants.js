3f3bc6ff8b56d97db17ffdbaf3f1c96669b07223
Giulia <giuliaecampana@gmail.com>
2019-10-22 12:10:52 -0700

    Remove PSA from products

--- src/containers/products/Constants.js
+++ src/containers/products/Constants.js
@@ -1,16 +1,13 @@
 // Products
 export const title1 = 'Holodeck';
-export const title2 = 'RideAlong';
-export const title3 = 'PSA';
-export const title4 = 'Pretrial Case Management';
-export const title5 = 'Chronicle';
+export const title2 = 'Care';
+export const title3 = 'Pretrial Case Management';
+export const title4 = 'Chronicle';
 
 export const body1 = (`Holodeck allows you to explore your data — search a dataset, traverse connections between
   datasets, identify top utilizers, and perform analysis.`);
 export const body2 = (`A tool for collecting behavioral health report data, viewing historical data, and
   performing analysis.`);
-export const body3 = (`Use the PSA calculator to compute a person's risk of failing to appear in court,
-  engaging in new criminal activity, and committing a violent crime if released until their court date.`);
-export const body4 = (`Manage a person as they move through the pretrial court system, from a public safety assessment,
+export const body3 = (`Manage a person as they move through the pretrial court system, from a public safety assessment,
   to scheduling an initial appearance hearing, to tracking their outcomes given a judge's decision.`);
-export const body5 = 'Create and manage studies, participants, and devices, and access study data.';
+export const body4 = 'Create and manage studies, participants, and devices, and access study data.';
