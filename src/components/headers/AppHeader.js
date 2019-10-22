5a8c3f9ea6f656d6087cc9f6d63c8978312ab46c
Giulia <giuliaecampana@gmail.com>
2019-10-22 15:29:23 -0700

    create active link styles using NavLink

--- src/components/headers/AppHeader.js
+++ src/components/headers/AppHeader.js
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo-white.png';
 import { WINDOW_EDGE_PADDING } from '../../core/style/Sizes';
@@ -10,6 +10,11 @@ import * as Routes from '../../core/router/Routes';
 
 const helpLink = 'https://help.openlattice.com';
 const scheduleACallLink = 'https://calendly.com/openlattice/openlattice-web-request';
+
+const activeLinkStyles = {
+  color: N0,
+  fontWeight: 600,
+};
 
 const Header = styled.div`
   display: flex;
@@ -25,7 +30,7 @@ const Menu = styled.div`
   justify-content: space-between;
 `;
 
-const MenuInternalLink = styled(Link)`
+const MenuInternalLink = styled(NavLink)`
   color: ${N5};
   margin-right: 30px;
   text-decoration: none;
@@ -58,7 +63,7 @@ const AppHeader = () => (
       <img src={logoImg} alt="OpenLattice Logo" height={50} />
     </Link>
     <Menu>
-      <MenuInternalLink to={Routes.PRODUCTS}>Products</MenuInternalLink>
+      <MenuInternalLink activeStyle={activeLinkStyles} to={Routes.PRODUCTS}>Products</MenuInternalLink>
       <MenuExternalLink href={helpLink} target="_blank">Help</MenuExternalLink>
       <ScheduleACallButton href={scheduleACallLink} target="_blank">Schedule a call</ScheduleACallButton>
     </Menu>
