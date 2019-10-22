36723eb645a6d5112564e86f97d8f8d2290219cc
Giulia <giuliaecampana@gmail.com>
2019-10-22 14:38:23 -0700

    update routes

--- src/core/router/Routes.js
+++ src/core/router/Routes.js
@@ -5,10 +5,19 @@
 export const ROOT :string = '/';
 export const LOGIN :string = '/login';
 export const PRODUCTS :string = '/products';
+export const PLATFORM :string = '/platform';
 
-export const HOLODECK :string = `${PRODUCTS}/holodeck`;
-export const RIDE_ALONG :string = `${PRODUCTS}/ridealong`;
-export const PSA :string = `${PRODUCTS}/psa`;
+export const REFERRALS :string = `${PRODUCTS}/referrals`;
+export const CARE :string = `${PRODUCTS}/care`;
 export const PCM :string = `${PRODUCTS}/pretrialcasemanagement`;
+export const ASTROMETRICS :string = `${PRODUCTS}/astrometrics`;
 export const CHRONICLE :string = `${PRODUCTS}/chronicle`;
-export const ECR :string = `${PRODUCTS}/ecr`;
+export const CWP :string = `${PRODUCTS}/cwp`;
+export const STEPPING_UP :string = `${PRODUCTS}/steppingup`;
+export const HOLODECK :string = `${PRODUCTS}/holodeck`;
+export const PSA :string = `${PRODUCTS}/psa`;
+
+export const GALLERY :string = `${PLATFORM}/gallery`;
+export const ORGANIZATIONS :string = `${PLATFORM}/organizations`;
+export const RESEARCH_HUB :string = `${PLATFORM}/researchhub`;
+export const OTHER_SERVICES :string = `${PLATFORM}/otherservices`;
