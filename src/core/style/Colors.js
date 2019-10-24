76da5b216115b1b668dcf86658575f79e9208220
Giulia <giuliaecampana@gmail.com>
2019-10-23 18:09:51 -0700

    add new colors

--- src/core/style/Colors.js
+++ src/core/style/Colors.js
@@ -12,6 +12,10 @@ export const N2 :string = '#6f788a';
 export const N3 :string = '#4d4d4d';
 export const N4 :string = '#000000';
 export const N5 :string = '#b9b7bf';
+export const N6 :string = '#F9F9FC';
+export const N7 :string = '#3B3B49';
+export const N8 :string = '#505060';
+export const N9 :string = '#9999AF';
 
 /*
 * pinks
@@ -30,3 +34,4 @@ export const PP3 :string = '#9359ff';
 export const PP4 :string = '#6124e2';
 export const PP5 :string = '#31275c';
 export const PP6 :string = '#7c7791';
+export const PP7 :string = '#7860FF';
