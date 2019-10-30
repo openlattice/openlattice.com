8a67111239050ae1c73beedfa6ceec2e81e9522e
Giulia <giuliaecampana@gmail.com>
2019-10-30 10:51:13 -0700

    other pages

---
+++ src/containers/products/OtherProductsPage.js
@@ -0,0 +1,14 @@
+/*
+ * @flow
+ */
+import React from 'react';
+
+import ComingSoon from '../../components/placeholder/ComingSoon';
+
+const OtherProductsPage = () => (
+  <>
+    <ComingSoon />
+  </>
+);
+
+export default OtherProductsPage;
