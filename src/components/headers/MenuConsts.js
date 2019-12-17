04a66a566ca40c8403bc5c0dda45af5f8650d34b
Giulia <giuliaecampana@gmail.com>
2019-12-17 15:28:07 -0800

    consts for menu names and routes

---
+++ src/components/headers/MenuConsts.js
@@ -0,0 +1,30 @@
+// @flow
+
+import * as Routes from '../../core/router/Routes';
+
+const helpCenterLink :string = 'https://help.openlattice.com';
+const scheduleACallLink :string = 'https://calendly.com/openlattice/openlattice-web-request';
+
+const MENU_HEADERS :Object = {
+  ABOUT: 'About',
+  WORKING_WITH_US: 'Working with us',
+  PLATFORM: 'Platform',
+  PRODUCTS: 'Products',
+  HELP_CENTER: 'Help Center',
+  SCHEDULE_A_CALL: 'Schedule a Call'
+};
+
+const MENU_ROUTES :Object = {
+  ABOUT: Routes.ABOUT,
+  WORKING_WITH_US: Routes.WORKING_WITH_US,
+  PLATFORM: Routes.PLATFORM,
+  PRODUCTS: Routes.PRODUCTS,
+  HELP_CENTER: helpCenterLink,
+  ROOT: Routes.ROOT,
+  SCHEDULE_A_CALL: scheduleACallLink
+};
+
+export {
+  MENU_HEADERS,
+  MENU_ROUTES
+};
