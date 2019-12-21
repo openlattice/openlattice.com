14a27afec99dd3f7184887c06d241f1cce985dce
Giulia <giuliaecampana@gmail.com>
2019-12-20 17:52:22 -0800

    add list items to ComplianceAndSecurity

--- src/containers/platform/ComplianceAndSecurity.js
+++ src/containers/platform/ComplianceAndSecurity.js
@@ -5,11 +5,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import IconListItem from './security/IconListItem';
 import PageSection from '../../components/layout/NewPageSection';
 
 import { SectionHeader } from '../../components/headers/PageHeaders';
 import { NEUTRALS } from '../../core/style/Colors';
 import { CONTENT_WIDTH, MEDIA_QUERY_LG, MEDIA_QUERY_MD } from '../../core/style/Sizes';
+import {
+  COMPLIANCE_DESCRIPTIONS,
+  COMPLIANCE_ICONS,
+  COMPLIANCE_TITLES,
+  SECURITY_DESCRIPTIONS,
+  SECURITY_ICONS,
+  SECURITY_TITLES
+} from './security/ComplianceSecurityConsts';
 
 const Content = styled.div`
   align-items: center;
@@ -50,6 +59,28 @@ const Line = styled.div`
   width: 100%;
 `;
 
+const ListOuterWrapper = styled.div`
+  display: flex;
+  flex: 1 0 auto;
+  flex-direction: row;
+  justify-content: center;
+  margin: auto;
+  margin-top: 41px;
+  margin-bottom: 101px;
+  max-width: ${CONTENT_WIDTH}px;
+`;
+
+const ListInnerWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: -24px;
+
+  @media only screen and (min-width: ${MEDIA_QUERY_LG}px) {
+    justify-content: flex-start;
+  }
+`;
+
 const ComplianceAndSecurity = () => (
   <PageSection bgColor={NEUTRALS.WHITE}>
     <Content>
@@ -59,12 +90,52 @@ const ComplianceAndSecurity = () => (
         </SmallSectionHeader>
         <Line />
       </SectionHeaderWrapper>
+      <ListOuterWrapper>
+        <ListInnerWrapper>
+          <IconListItem
+              body={COMPLIANCE_DESCRIPTIONS.CJIS}
+              icon={COMPLIANCE_ICONS.CJIS}
+              title={COMPLIANCE_TITLES.CJIS} />
+          <IconListItem
+              body={COMPLIANCE_DESCRIPTIONS.GDPR}
+              icon={COMPLIANCE_ICONS.GDPR}
+              title={COMPLIANCE_TITLES.GDPR} />
+          <IconListItem
+              body={COMPLIANCE_DESCRIPTIONS.HIPAA}
+              icon={COMPLIANCE_ICONS.HIPAA}
+              title={COMPLIANCE_TITLES.HIPAA} />
+          <IconListItem
+              body={COMPLIANCE_DESCRIPTIONS.CFR}
+              icon={COMPLIANCE_ICONS.CFR}
+              title={COMPLIANCE_TITLES.CFR} />
+          <IconListItem
+              body={COMPLIANCE_DESCRIPTIONS.FERPA}
+              icon={COMPLIANCE_ICONS.FERPA}
+              title={COMPLIANCE_TITLES.FERPA} />
+        </ListInnerWrapper>
+      </ListOuterWrapper>
       <SectionHeaderWrapper>
         <SmallSectionHeader>
           Security
         </SmallSectionHeader>
         <Line />
       </SectionHeaderWrapper>
+      <ListOuterWrapper>
+        <ListInnerWrapper>
+          <IconListItem
+              body={SECURITY_DESCRIPTIONS.ENCRYPTION}
+              icon={SECURITY_ICONS.ENCRYPTION}
+              title={SECURITY_TITLES.ENCRYPTION} />
+          <IconListItem
+              body={SECURITY_DESCRIPTIONS.LOGIN}
+              icon={SECURITY_ICONS.LOGIN}
+              title={SECURITY_TITLES.LOGIN} />
+          <IconListItem
+              body={SECURITY_DESCRIPTIONS.CLOUD}
+              icon={SECURITY_ICONS.CLOUD}
+              title={SECURITY_TITLES.CLOUD} />
+        </ListInnerWrapper>
+      </ListOuterWrapper>
     </Content>
   </PageSection>
 );
