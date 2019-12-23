ac15615f3f54c3befcec650d7edb5378cc2d83d8
Giulia <giuliaecampana@gmail.com>
2019-12-23 13:28:43 -0600

    New products pagE

---
+++ src/containers/products/NewProductsPage.js
@@ -0,0 +1,15 @@
+/*
+ * @flow
+ */
+
+import React from 'react';
+
+import ProductsIntro from './ProductsIntro';
+
+const Products = () => (
+  <>
+    <ProductsIntro />
+  </>
+);
+
+export default Products;
