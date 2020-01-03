1f95de28772c0a491589e6e47ad60a4569c1d29f
Giulia <giuliaecampana@gmail.com>
2020-01-03 15:49:34 -0800

    add everything to Working with us page

--- src/containers/workingwithus/WorkingWithUs.js
+++ src/containers/workingwithus/WorkingWithUs.js
@@ -4,8 +4,18 @@
 
 import React from 'react';
 
+import WorkingWithUsIntro from './WorkingWithUsIntro';
+import WorkingWithUsProcess from './WorkingWithUsProcess';
+import ContactUsSection from '../contactus/ContactUsSection';
+import FooterSectionWithPageLinks from '../footer/FooterSectionWithPageLinks';
+
 const WorkingWithUs = () => (
-  <div />
+  <>
+    <WorkingWithUsIntro />
+    <WorkingWithUsProcess />
+    <ContactUsSection />
+    <FooterSectionWithPageLinks />
+  </>
 );
 
 export default WorkingWithUs;
