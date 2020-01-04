d55b6c97e4556e120406f1048600446e88a2a79c
Giulia <giuliaecampana@gmail.com>
2020-01-03 18:13:44 -0800

    terms of service text

---
+++ src/containers/footer/termsofservice/Text.js
@@ -0,0 +1,79 @@
+// @flow
+
+const INTRODUCTION :Object = {
+  PARAGRAPH_2: (`Capitalized terms not defined in these Terms of Use shall have the meaning set forth in
+    our Privacy Policy.`),
+  PARAGRAPH_3: (`THE SECTIONS BELOW TITLED “BINDING ARBITRATION” AND “CLASS ACTION WAIVER” CONTAIN A BINDING
+    ARBITRATION AGREEMENT AND CLASS ACTION WAIVER. THEY AFFECT YOUR LEGAL RIGHTS. PLEASE READ THEM.`)
+};
+
+const ITEM_1 :Object = {
+  TITLE: '1. Use of Personal Information'
+};
+
+const ITEM_3 :Object = {
+  PARAGRAPH_1: (`When you or your organization choose to upload or integrate data (“Content”) onto the Platform,
+    you or your organization affirm that the Content complies with Site Guidelines above. You or your organization
+    retain ownership rights over Content and only grant to OpenLattice the limited rights required to offer the
+    Services. These limited rights include permission to access, transfer, copy, process, analyze and utilize
+    the Content in any manner reasonably necessary to manage and administer the Platform and improve the
+    Platform and our Services.`),
+  PARAGRAPH_2: (`Our Services and the Platform include features that allow you or your organization to share
+    Content with third parties via the Platform. Content is private by default but can be shared by granting
+    third parties permissions that allow access to Content. You or your organization are solely responsible
+    for (1) determining whether a given third party has an appropriate need to access Content, (2) entering
+    into an agreement with that third party to comply with any and all laws, regulations, rules, or polices
+    pertaining to the Content, and (3) configuring or requesting OpenLattice assistance in configuring
+    permissions to grant to that third party access to Content.`),
+  TITLE: '3. Customer Content, Permissions, and Sharing'
+};
+
+const ITEM_4 :Object = {
+  PARAGRAPH_1: (`The Site contains materials, such as software, text, graphics, images, sound recordings,
+    audiovisual works, webinars, white papers, case studies, and other materials provided by or on behalf
+    of OpenLattice (collectively referred to as the “Materials”). The Materials may be owned by us or by
+    third parties. The Materials are protected under both United States and foreign laws. Unauthorized
+    use of the Materials may violate copyright, trademark, and other laws. You have no rights in or to
+    the Materials, and you will not use the Materials except as permitted under this Agreement. No other
+    use is permitted without prior written consent from us. You must retain all copyright and other
+    proprietary notices contained in the original Materials on any copy you make of the Materials.
+    You may not sell, transfer, assign, license, sublicense, or modify the Materials or reproduce,
+    display, publicly perform, make a derivative version of, distribute, or otherwise use the Materials
+    in any way for any public or commercial purpose. The use or posting of the Materials on any other
+    website or in a networked computer environment for any purpose is expressly prohibited.`),
+  PARAGRAPH_2: (`If you violate any part of this Agreement, your permission to access and/or use the
+    Materials and the Site automatically terminates and you must immediately destroy any copies you
+    have made of the Materials.`),
+  PARAGRAPH_3: (`The trademarks, service marks, and logos of OpenLattice (“OpenLattice Trademarks”) used
+    and displayed on the Site are registered and unregistered trademarks or service marks of OpenLattice.
+    Other company, product, and service names located on the Site may be trademarks or service marks owned
+    by others (the “Third-Party Trademarks”, and, collectively with the OpenLattice Trademarks, the
+    “Trademarks”). Nothing on the Site should be construed as granting, by implication, estoppel,
+    or otherwise, any license or right to use the Trademarks, without our prior written permission
+    specific for each such use. Use of the Trademarks as part of a link to or from any site is
+    prohibited unless establishment of such a link is approved in advance by us in writing.
+    All goodwill generated from the use of the OpenLattice Trademarks inures to our benefit.`),
+  PARAGRAPH_4: (`Elements of the Site are protected by trade dress, trademark, unfair competition,
+    and other state and federal laws and may not be copied or imitated in whole or in part, by any
+    means, including but not limited to the use of framing or mirrors. None of the Materials may
+    be retransmitted without our express, written consent for each and every instance.`),
+  TITLE: '4. Intellectual Property'
+};
+
+const ITEM_5 :Object = {
+  BODY: (`We encourage you to e-mail us, but we do not want you to, and you should not, e-mail us any
+    content that contains confidential information. With respect to all e-mails you send to us,
+    including but not limited to, feedback, questions, comments, suggestions, and the like, we
+    shall be free to use any ideas, concepts, know-how, or techniques contained in your communications
+    for any purpose whatsoever, including but not limited to, the development, production and marketing
+    of products and services that incorporate such information.`),
+  TITLE: '5. Communications with Us'
+};
+
+export {
+  INTRODUCTION,
+  ITEM_1,
+  ITEM_3,
+  ITEM_4,
+  ITEM_5
+};
