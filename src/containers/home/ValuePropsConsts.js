53a081ccc8cf883bb5df3cf1e6a67a92fba8d24b
Giulia <giuliaecampana@gmail.com>
2020-01-08 12:29:27 -0800

    Value props consts

---
+++ src/containers/home/ValuePropsConsts.js
@@ -0,0 +1,21 @@
+// @flow
+
+const VALUE_PROP_HEADERS = {
+  SECURE: ['Secure', 'data integration'],
+  ACTIONABLE: ['Actionable data for', 'social policy'],
+  TRANSPARENCY: ['Transparency and', 'data integrity']
+};
+
+const VALUE_PROP_DESCRIPTIONS = {
+  SECURE: (`End-to-end encryption ensures data is securely stored at all times,
+    both in our database and in transit.`),
+  ACTIONABLE: (`Placeholder - standardized data sets for jail, police, and rehab
+    center data allow us to integrate with government agency...`),
+  TRANSPARENCY: (`Tamper-proof audit trail ensures every search, read, and write is
+    logged, guarding against unlawful or malicious use.`)
+};
+
+export {
+  VALUE_PROP_DESCRIPTIONS,
+  VALUE_PROP_HEADERS,
+};
