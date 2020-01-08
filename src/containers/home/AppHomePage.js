86e74f57f82b83e88460e6aee795c5036f74c688
Giulia <giuliaecampana@gmail.com>
2020-01-08 12:29:48 -0800

    new app home page with 2 new sections

---
+++ src/containers/home/AppHomePage.js
@@ -0,0 +1,17 @@
+/*
+ * @flow
+ */
+
+import React from 'react';
+
+import IntroSection from './IntroSection';
+import ValueProps from './ValueProps';
+
+const AppHomePage = () => (
+  <>
+    <IntroSection />
+    <ValueProps />
+  </>
+);
+
+export default AppHomePage;
