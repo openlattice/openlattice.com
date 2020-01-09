06c3c78958f43e669ebd9b53ca1a4f5ffa3ee59d
Giulia <giuliaecampana@gmail.com>
2020-01-08 17:00:30 -0800

    add our customers section to app home page

--- src/containers/home/AppHomePage.js
+++ src/containers/home/AppHomePage.js
@@ -7,12 +7,14 @@ import React from 'react';
 import Intro from './Intro';
 import ValueProps from './ValueProps';
 import Features from './Features';
+import OurCustomers from './OurCustomers';
 
 const AppHomePage = () => (
   <>
     <Intro />
     <ValueProps />
     <Features />
+    <OurCustomers />
   </>
 );
 
