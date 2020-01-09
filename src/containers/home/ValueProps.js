6fe737c928c9d141385a21975b9fdc8191903967
Giulia <giuliaecampana@gmail.com>
2020-01-09 10:37:55 -0800

    make value props responsive

--- src/containers/home/ValueProps.js
+++ src/containers/home/ValueProps.js
@@ -17,14 +17,14 @@ import {
   MEDIA_QUERY_LG,
   MEDIA_QUERY_TECH_SM
 } from '../../core/style/Sizes';
-import { VALUE_PROP_DESCRIPTIONS, VALUE_PROP_HEADERS } from './ValuePropsConsts';
+import { VALUE_PROP_DESCRIPTIONS, VALUE_PROP_HEADERS, VALUE_PROP_ICONS } from './ValuePropsConsts';
 
 const Content = styled.div`
   align-items: center;
   display: flex;
   flex-direction: column;
-  margin: 50px auto 100px auto;
-  max-width: 100%;
+  margin: 50px 0 100px 0;
+  width: 100%;
   text-align: center;
 
   @media only screen and (min-width: ${MEDIA_QUERY_LG}px) {
@@ -35,10 +35,10 @@ const Content = styled.div`
 const ValuePropsOuterWrapper = styled.div`
   align-items: center;
   display: flex;
-  flex: 1 0 auto;
   flex-direction: row;
   justify-content: center;
   margin: auto;
+  width: 100%;
 
   @media only screen and (min-width: ${MEDIA_QUERY_MD}px) {
     margin: 0 -24px;
@@ -51,6 +51,12 @@ const ValuePropsInnerWrapper = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   margin: -24px;
+  width: 100%;
+
+  @media only screen and (max-width: ${MEDIA_QUERY_TECH_SM}px) {
+    margin-right: 0;
+    margin-left: 0;
+  }
 
   @media only screen and (min-width: ${MEDIA_QUERY_LG}px) {
     justify-content: flex-start;
@@ -58,7 +64,7 @@ const ValuePropsInnerWrapper = styled.div`
 `;
 
 const ValuePropWrapper = styled.div`
-  height: 232px;
+  height: 295px;
   margin: 24px;
   position: relative;
   width: 100%;
@@ -66,12 +72,19 @@ const ValuePropWrapper = styled.div`
   @media only screen and (min-width: ${MEDIA_QUERY_TECH_SM}px) {
     width: 336px;
   }
+
+  @media only screen and (max-width: ${MEDIA_QUERY_TECH_SM}px) {
+    margin-right: 0;
+    margin-left: 0;
+  }
 `;
 
 const ValuePropHeader = styled(SectionHeader)`
   font-size: 25px;
   margin-bottom: 0;
   width: 100%;
+  top: 63px;
+  position: absolute;
 `;
 
 const Text = styled.div`
@@ -80,7 +93,7 @@ const Text = styled.div`
   line-height: 150%;
   text-align: center;
   position: absolute;
-  top: 88px;
+  top: 151px;
 `;
 
 const LearnMoreWrapper = styled(LearnMoreInternalLink)`
@@ -102,6 +115,7 @@ const ValueProps = () => (
       <ValuePropsOuterWrapper>
         <ValuePropsInnerWrapper>
           <ValuePropWrapper>
+            <img src={VALUE_PROP_ICONS.SECURE} alt="" />
             <ValuePropHeader>
               { VALUE_PROP_HEADERS.SECURE[0] }
               <br />
@@ -111,6 +125,7 @@ const ValueProps = () => (
             <LearnMoreWrapper to={Routes.ROOT}>Learn more</LearnMoreWrapper>
           </ValuePropWrapper>
           <ValuePropWrapper>
+            <img src={VALUE_PROP_ICONS.ACTIONABLE} alt="" />
             <ValuePropHeader>
               { VALUE_PROP_HEADERS.ACTIONABLE[0] }
               <br />
@@ -120,6 +135,7 @@ const ValueProps = () => (
             <LearnMoreWrapper to={Routes.ROOT}>Learn more</LearnMoreWrapper>
           </ValuePropWrapper>
           <ValuePropWrapper>
+            <img src={VALUE_PROP_ICONS.TRANSPARENCY} alt="" />
             <ValuePropHeader>
               { VALUE_PROP_HEADERS.TRANSPARENCY[0] }
               <br />
