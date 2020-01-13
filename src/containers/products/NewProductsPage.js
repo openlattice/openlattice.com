e35dc452d8535fb581486791f6cf992bfb0b47ac
Giulia <giuliaecampana@gmail.com>
2020-01-13 10:52:27 -0800

    add scroll to top

--- src/containers/products/NewProductsPage.js
+++ src/containers/products/NewProductsPage.js
@@ -8,9 +8,11 @@ import ContactUsSection from '../contactus/ContactUsSection';
 import FooterSection from '../footer/FooterSectionWithPageLinks';
 import ProductsIntro from './ProductsIntro';
 import ProductsList from './ProductsList';
+import ScrollToTopOnMount from '../../components/layout/ScrollToTopOnMount';
 
 const Products = () => (
   <>
+    <ScrollToTopOnMount />
     <ProductsIntro />
     <ProductsList />
     <ContactUsSection />
