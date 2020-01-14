fe63f81ef078bf12a04ee7dafaf7dcccc534d2b4
Giulia <giuliaecampana@gmail.com>
2020-01-14 12:16:56 -0800

    add research hub path to AppContainer

--- src/containers/app/AppContainer.js
+++ src/containers/app/AppContainer.js
@@ -13,12 +13,14 @@ import CareProductPage from '../products/CareProductPage';
 import PCMProductPage from '../products/PCMProductPage';
 import Products from '../products/ProductsPage';
 import Platform from '../platform/Platform';
+import ResearchHubProductPage from '../products/ResearchHubProductPage';
 import WorkingWithUs from '../workingwithus/WorkingWithUs';
 
 const AppContainer = () => (
   <Switch>
     <Route exact path={Routes.CARE} component={CareProductPage} />
     <Route exact path={Routes.PCM} component={PCMProductPage} />
+    <Route exact path={Routes.RESEARCH_HUB} component={ResearchHubProductPage} />
     <Route exact path={Routes.PRODUCTS} component={Products} />
     <Route exact path={Routes.PLATFORM} component={Platform} />
     <Route exact path={Routes.WORKING_WITH_US} component={WorkingWithUs} />
