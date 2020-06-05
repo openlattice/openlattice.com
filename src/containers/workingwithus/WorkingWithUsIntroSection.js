f8717b03c58a6f0c68d3dcf666cad6ec3286fc0f
Hristo Oskov <hristo@openlattice.com>
2020-06-05 15:04:05 -0700

    working with us intro section

--- src/containers/workingwithus/WorkingWithUsIntroSection.js
+++ src/containers/workingwithus/WorkingWithUsIntroSection.js
@@ -3,34 +3,31 @@
  */
 
 import React from 'react';
-import styled from 'styled-components';
 
 import PageSection from '../../components/layout/NewPageSection';
-
-import { PageIntro } from '../../components/headers/PageHeaders';
-import { Content, PageDescriptionWrapper } from '../../components/layout/IntroComponents';
-import { ContactUsPurple } from '../../components/controls/index';
+import {
+  ContactUsPurple,
+  Header,
+  SectionContent,
+  SubHeader,
+} from '../../components';
 import { NEUTRALS } from '../../core/style/Colors';
 import { openBeacon } from '../../utils/Utils';
 
-const PageIntroWrapper = styled(PageIntro)`
-  width: 816px;
-`;
+const INTRO_HEADER = 'Technology solutions to empower public servants';
+
+const INTRO_SUB_HEADER = 'Join dozens of jurisdictions across the United States who are using OpenLattice to better'
+  + ' serve their constituents.';
 
 const WorkingWithUsIntroSection = () => (
   <PageSection bgColor={NEUTRALS.GRAY_05} includeMenu>
-    <Content>
-      <PageIntroWrapper>
-        Technology solutions to empower public servants.
-      </PageIntroWrapper>
-      <PageDescriptionWrapper>
-        {`Join dozens of jurisdictions across the United States who are using OpenLattice to
-          better serve their constituents.`}
-      </PageDescriptionWrapper>
+    <SectionContent maxWidth={{ sm: 500, lg: 720 }}>
+      <Header>{INTRO_HEADER}</Header>
+      <SubHeader>{INTRO_SUB_HEADER}</SubHeader>
       <ContactUsPurple onClick={openBeacon}>
         Schedule a call
       </ContactUsPurple>
-    </Content>
+    </SectionContent>
   </PageSection>
 );
 
