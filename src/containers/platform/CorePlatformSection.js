63c2342a2d7dc4f2f8db9fb5843f981440d1346a
Hristo Oskov <hristo@openlattice.com>
2020-06-04 20:00:09 -0700

    fancy margins yo

--- src/containers/platform/CorePlatformSection.js
+++ src/containers/platform/CorePlatformSection.js
@@ -41,6 +41,7 @@ import {
   SubHeader,
 } from '../../components';
 import { NEUTRALS } from '../../core/style/Colors';
+import { MEDIA_QUERY_MD, MEDIA_QUERY_SM } from '../../core/style/Sizes';
 import {
   DATA_INTEGRATIONS_LINK,
   DATA_LINKING_LINK,
@@ -50,8 +51,27 @@ import {
   TOP_UTILIZERS_LINK,
 } from '../../utils/constants/links';
 
+// NOTE: the 328px value her is very carfully chosen and is tightly coupled with the media query below in Tile
 const TileGrid = styled(SectionContentGrid)`
-  grid-template-columns: repeat(auto-fit,minmax(336px,1fr));
+  @media only screen and (min-width: ${MEDIA_QUERY_SM}px) {
+    grid-template-columns: repeat(auto-fit,minmax(328px,1fr));
+  }
+
+  > div {
+    max-width: 384px;
+  }
+`;
+
+const Tile = styled(InfoTile)`
+  @media only screen and (min-width: ${MEDIA_QUERY_MD}px) {
+    &:nth-child(even) {
+      margin: 0 auto 0 0;
+    }
+
+    &:nth-child(odd) {
+      margin: 0 0 0 auto;
+    }
+  }
 `;
 
 const CorePlatformSection = () => (
@@ -62,42 +82,42 @@ const CorePlatformSection = () => (
     </SectionContent>
     <SectionContent>
       <TileGrid alignItems="stretch">
-        <InfoTile withBorder>
+        <Tile withBorder>
           <DBSyncCircleIcon width="50" />
           <Header as="h3">{DATA_INTEGRATION_HEADER}</Header>
           <SubHeader as="h6">{DATA_INTEGRATION_SUBHEADER}</SubHeader>
           <LearnMoreExternalLink href={DATA_INTEGRATIONS_LINK} target="_blank">Learn more</LearnMoreExternalLink>
-        </InfoTile>
-        <InfoTile withBorder>
+        </Tile>
+        <Tile withBorder>
           <LinkCircleIcon width="50" />
           <Header as="h3">{DATA_LINKING_HEADER}</Header>
           <SubHeader as="h6">{DATA_LINKING_SUBHEADER}</SubHeader>
           <LearnMoreExternalLink href={DATA_LINKING_LINK} target="_blank">Learn more</LearnMoreExternalLink>
-        </InfoTile>
-        <InfoTile withBorder>
+        </Tile>
+        <Tile withBorder>
           <CloudSecureCircleIcon width="50" />
           <Header as="h3">{DATA_STORAGE_HEADER}</Header>
           <SubHeader as="h6">{DATA_STORAGE_SUBHEADER}</SubHeader>
           <LearnMoreExternalLink href={PRIVACY_SECURITY_LINK} target="_blank">Learn more</LearnMoreExternalLink>
-        </InfoTile>
-        <InfoTile withBorder>
+        </Tile>
+        <Tile withBorder>
           <SearchCircleIcon width="50" />
           <Header as="h3">{DATA_EXPLORATION_HEADER}</Header>
           <SubHeader as="h6">{DATA_EXPLORATION_SUBHEADER}</SubHeader>
           <LearnMoreExternalLink href={TOP_UTILIZERS_LINK} target="_blank">Learn more</LearnMoreExternalLink>
-        </InfoTile>
-        <InfoTile withBorder>
+        </Tile>
+        <Tile withBorder>
           <DotGridCircleIcon width="50" />
           <Header as="h3">{DATA_STANDARDIZATION_HEADER}</Header>
           <SubHeader as="h6">{DATA_STANDARDIZATION_SUBHEADER}</SubHeader>
           <LearnMoreExternalLink href={EDM_LINK} target="_blank">Learn more</LearnMoreExternalLink>
-        </InfoTile>
-        <InfoTile withBorder>
+        </Tile>
+        <Tile withBorder>
           <KeyCircleIcon width="50" />
           <Header as="h3">{PERMISSIONS_MANAGEMENT_HEADER}</Header>
           <SubHeader as="h6">{PERMISSIONS_MANAGEMENT_SUBHEADER}</SubHeader>
           <LearnMoreExternalLink href={MANAGE_PERMISSIONS_LINK} target="_blank">Learn more</LearnMoreExternalLink>
-        </InfoTile>
+        </Tile>
       </TileGrid>
     </SectionContent>
   </PageSection>
