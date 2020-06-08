a0eec05400e94859894219bbc55bf7597df47418
Hristo Oskov <hristo@openlattice.com>
2020-06-08 11:45:36 -0700

    products intro section

--- src/containers/products/ProductsIntroSection.js
+++ src/containers/products/ProductsIntroSection.js
@@ -3,39 +3,31 @@
  */
 
 import React from 'react';
-import styled from 'styled-components';
 
 import PageSection from '../../components/layout/NewPageSection';
-
-import { PageIntro } from '../../components/headers/PageHeaders';
-import { Content, PageDescriptionWrapper } from '../../components/layout/IntroComponents';
-import { ContactUsPurple } from '../../components/controls/index';
+import {
+  ContactUsPurple,
+  Header,
+  SectionContent,
+  SubHeader,
+} from '../../components';
 import { NEUTRALS } from '../../core/style/Colors';
-import { MEDIA_QUERY_MD } from '../../core/style/Sizes';
 import { openBeacon } from '../../utils/Utils';
 
-const PageIntroWrapper = styled(PageIntro)`
-  width: 720px;
-
-  @media only screen and (max-width: ${MEDIA_QUERY_MD}px) {
-    width: 100%;
-  }
+const INTRO_HEADER = `Solve modern problems
+with modern solutions
 `;
+
+const INTRO_SUB_HEADER = `Use data-driven workflows to accurately and efficiently identify
+problems, take necessary actions, and track outcomes.`;
 
 const ProductsIntroSection = () => (
   <PageSection bgColor={NEUTRALS.GRAY_05} includeMenu>
-    <Content>
-      <PageIntroWrapper>
-        Solve modern problems with modern solutions.
-      </PageIntroWrapper>
-      <PageDescriptionWrapper>
-        {`Use data-driven workflows to accurately and efficiently identify problems,
-          take necessary actions, and track outcomes.`}
-      </PageDescriptionWrapper>
-      <ContactUsPurple onClick={openBeacon}>
-        Schedule a call
-      </ContactUsPurple>
-    </Content>
+    <SectionContent maxWidth={{ sm: 480, md: 720 }}>
+      <Header>{INTRO_HEADER}</Header>
+      <SubHeader>{INTRO_SUB_HEADER}</SubHeader>
+      <ContactUsPurple onClick={openBeacon}>Schedule a call</ContactUsPurple>
+    </SectionContent>
   </PageSection>
 );
 
