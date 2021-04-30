08b3c4afeb36e32f25d439b1ec5a01b412a3cd08
Hristo Oskov <hristo@openlattice.com>
2021-04-30 12:09:58 -0700

    remove folks who are no longer with us

--- src/assets/team/index.js
+++ src/assets/team/index.js
@@ -4,66 +4,45 @@
 
 import styled from 'styled-components';
 
-import AndreaPNG from './andrea.png';
 import BlakePNG from './blake.png';
 import BrandonPNG from './brandon.png';
-import DavidPNG from './david.png';
 import DrewPNG from './drew.png';
 import GiuliaPNG from './giulia.png';
 import HristoPNG from './hristo.png';
-import JasonPNG from './jason.png';
 import JoePNG from './joe.png';
-import JokePNG from './joke.png';
-import KatherinePNG from './katherine.png';
+import KatherineJPG from './katherine.jpg';
 import KimPNG from './kim.png';
-import LynnPNG from './lynn.png';
 import MandyPNG from './mandy.png';
 import MatthewPNG from './matthew.png';
-import MolliePNG from './mollie.png';
-import NicholasPNG from './nicholas.png';
 import ShireenPNG from './shireen.png';
 import SolomonPNG from './solomon.png';
 import ToddPNG from './todd.png';
 
-const AndreaPhoto = styled.img.attrs({ src: AndreaPNG })``;
 const BlakePhoto = styled.img.attrs({ src: BlakePNG })``;
 const BrandonPhoto = styled.img.attrs({ src: BrandonPNG })``;
-const DavidPhoto = styled.img.attrs({ src: DavidPNG })``;
 const DrewPhoto = styled.img.attrs({ src: DrewPNG })``;
 const GiuliaPhoto = styled.img.attrs({ src: GiuliaPNG })``;
 const HristoPhoto = styled.img.attrs({ src: HristoPNG })``;
-const JasonPhoto = styled.img.attrs({ src: JasonPNG })``;
 const JoePhoto = styled.img.attrs({ src: JoePNG })``;
-const JokePhoto = styled.img.attrs({ src: JokePNG })``;
-const KatherinePhoto = styled.img.attrs({ src: KatherinePNG })``;
+const KatherinePhoto = styled.img.attrs({ src: KatherineJPG })``;
 const KimPhoto = styled.img.attrs({ src: KimPNG })``;
-const LynnPhoto = styled.img.attrs({ src: LynnPNG })``;
 const MandyPhoto = styled.img.attrs({ src: MandyPNG })``;
 const MatthewPhoto = styled.img.attrs({ src: MatthewPNG })``;
-const MolliePhoto = styled.img.attrs({ src: MolliePNG })``;
-const NicholasPhoto = styled.img.attrs({ src: NicholasPNG })``;
 const ShireenPhoto = styled.img.attrs({ src: ShireenPNG })``;
 const SolomonPhoto = styled.img.attrs({ src: SolomonPNG })``;
 const ToddPhoto = styled.img.attrs({ src: ToddPNG })``;
 
 export {
-  AndreaPhoto,
   BlakePhoto,
   BrandonPhoto,
-  DavidPhoto,
   DrewPhoto,
   GiuliaPhoto,
   HristoPhoto,
-  JasonPhoto,
   JoePhoto,
-  JokePhoto,
   KatherinePhoto,
   KimPhoto,
-  LynnPhoto,
   MandyPhoto,
   MatthewPhoto,
-  MolliePhoto,
-  NicholasPhoto,
   ShireenPhoto,
   SolomonPhoto,
   ToddPhoto,
