001b5fb445abef974ba515cb0ae1cc99dcb90e11
Hristo Oskov <hristo@openlattice.com>
2021-07-26 11:44:18 -0700

    add Yue, Reka to team page

--- src/assets/team/index.js
+++ src/assets/team/index.js
@@ -13,8 +13,10 @@ import JoePNG from './joe.png';
 import KimPNG from './kim.png';
 import MandyPNG from './mandy.png';
 import MatthewPNG from './matthew.png';
+import RekaPNG from './reka.png';
 import SolomonPNG from './solomon.png';
 import ToddPNG from './todd.png';
+import YuePNG from './yue.png';
 
 const AlfoncePhoto = styled.img.attrs({ src: AlfonceJPG })``;
 const BlakePhoto = styled.img.attrs({ src: BlakePNG })``;
@@ -25,8 +27,10 @@ const JoePhoto = styled.img.attrs({ src: JoePNG })``;
 const KimPhoto = styled.img.attrs({ src: KimPNG })``;
 const MandyPhoto = styled.img.attrs({ src: MandyPNG })``;
 const MatthewPhoto = styled.img.attrs({ src: MatthewPNG })``;
+const RekaPhoto = styled.img.attrs({ src: RekaPNG })``;
 const SolomonPhoto = styled.img.attrs({ src: SolomonPNG })``;
 const ToddPhoto = styled.img.attrs({ src: ToddPNG })``;
+const YuePhoto = styled.img.attrs({ src: YuePNG })``;
 
 export {
   AlfoncePhoto,
@@ -38,6 +42,8 @@ export {
   KimPhoto,
   MandyPhoto,
   MatthewPhoto,
+  RekaPhoto,
   SolomonPhoto,
   ToddPhoto,
+  YuePhoto,
 };
