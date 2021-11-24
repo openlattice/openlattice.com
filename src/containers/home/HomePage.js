b8ad25d1f9cdf9e01a3a90e4a9f770326fb76315
Giulia Campana <32921059+giuliacampana@users.noreply.github.com>
2021-11-24 14:58:11 -0600

    make home page text edits

--- src/containers/home/HomePage.js
+++ src/containers/home/HomePage.js
@@ -21,7 +21,6 @@ import {
   LinkCircleIcon,
   SearchCircleIcon,
 } from '../../assets/svg/circle-icons';
-import { PlatformStackGraphic } from '../../assets/svg/graphics';
 import {
   ContentGrid,
   FeatureGrid,
@@ -71,12 +70,11 @@ const HomePage = () => (
       <SectionContent>
         <FeatureGrid>
           <Tile>
-            <Typography variant="h1">
-              Modern tools for modern research.
+            <Typography maxWidth={{ lg: 565 }} variant="h1">
+              The end-to-end platform for behavioral research
             </Typography>
             <Typography maxWidth={{ lg: 480 }} variant="body2">
-              Bridge the gap between data collection and analysis and expand your research possiblities with Chronicle’s
-              streamlined workflows and integrations.
+              Spend less time on administration and more time on science.
             </Typography>
             <SolidButton onClick={openBeacon}>Learn more</SolidButton>
           </Tile>
@@ -90,17 +88,15 @@ const HomePage = () => (
       <SectionContent>
         <FeatureGrid>
           <Tile>
-            <Typography maxWidth={{ lg: 576 }} variant="h2">
-              Collect, Analyze and Advance Your Research with Chronicle.
+            <Typography maxWidth={{ lg: 576 }} textAlign="center" variant="h2">
+              Streamline your research study with Chronicle
             </Typography>
           </Tile>
           <Tile>
             <Typography maxWidth={{ lg: 528 }} variant="body2">
-              We’re creating new ways for academic researchers to collect, analyze and advance their research with
-              Chronicle - a modern  Study Management tool that expands research capabilities. Chronicle enables
-              researchers and primary investigators to better understand participant behavior and unlocks new insights
-              with mobile functionality, ecological momentary assessment features, and customizable study management
-              workflows.
+              Enroll participants, collect consent, track protocol compliance, compensate participants, and
+              integrate passive passive sensing, experience sampling, surveys and other data sources for
+              analysis in one secure platform.
             </Typography>
           </Tile>
         </FeatureGrid>
@@ -110,44 +106,43 @@ const HomePage = () => (
           <Tile align={{ h: 'start' }}>
             <DBSyncCircleIcon width="50" />
             <Typography textAlign="left" variant="h6">
-              Discover More
+              Manage Participants
             </Typography>
             <Typography textAlign="left" variant="body1">
-              Enable participants to share data through a simple mobile application and access new streams of
-              information, such as time use diaries and 3rd party sensors.
+              Enroll, collect consent, track protocol compliance, and easily compensate participants in your study.
             </Typography>
             <LearnMoreButton href={PRODUCTS_CHRONICLE} />
           </Tile>
           <Tile align={{ h: 'start' }}>
             <LinkCircleIcon width="50" />
             <Typography textAlign="left" variant="h6">
-              Real-time Insights
+              Integrated Data Collection
             </Typography>
             <Typography textAlign="left" variant="body1">
-              {`Advance your research using our ecological momentary assessment features to measure participant's
-              behaviors and experiences in real-time.`}
+              Bring all your data together in one place whether you are using our survey and passive sensing tools or
+              third-party solutions for ecological momentary assessment or experience sampling.
             </Typography>
             <LearnMoreButton href={PRODUCTS_CHRONICLE} />
           </Tile>
           <Tile align={{ h: 'start' }}>
             <CloudSecureCircleIcon width="50" />
             <Typography textAlign="left" variant="h6">
-              Simplified Study Management
+              Automated Reporting
             </Typography>
             <Typography textAlign="left" variant="body1">
-              Streamline participant enrollment and participation with study management features and researcher
-              roles and permissions.
+              Researcher dashboard automatically notifies you of any irregularities in data collections and lets you
+              easily generate reports for oversight bodies such as IRBs and to comply with 45 CFR Part 46.
             </Typography>
             <LearnMoreButton href={PRODUCTS_CHRONICLE} />
           </Tile>
           <Tile align={{ h: 'start' }}>
             <SearchCircleIcon width="50" />
             <Typography textAlign="left" variant="h6">
-              Improved Accuracy
+              Secure Platform
             </Typography>
             <Typography textAlign="left" variant="body1">
-              Standardized data collection that can be viewed in a clean, simple, and intuitive way on our Chronicle
-              Dashboard or exported to a preferred statistical tool to increase study confidence.
+              Our platform meets standards for GDPR, HIPAA, 42 CFR Part 2, CJIS, FERPA, and has successfully completed
+              SOC2 Type II with CSA Star mappings for all 5 categories.
             </Typography>
             <LearnMoreButton href={PRODUCTS_CHRONICLE} />
           </Tile>
