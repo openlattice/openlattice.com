9c69ad393101af8eb2f8e8e75403dfb5d7b51f90
Hristo Oskov <hristo@openlattice.com>
2022-04-19 21:52:01 -0700

    Revert "Merge pull request #1026 from openlattice/develop"
    This reverts commit 96aa900fa02b227bac97aaedee36a67f49ad1812, reversing
    changes made to a0b60303ce58d652d3d90098b43c9f8f35876f1f.

--- src/containers/about/constants/index.js
+++ src/containers/about/constants/index.js
@@ -33,13 +33,13 @@ const ALFONCE :TeamMemberType = {
 
 const BLAKE :TeamMemberType = {
   NAME: 'Blake Freitas',
-  ROLE: 'Product Manager',
+  ROLE: 'Product Management Lead',
   PHOTO: <BlakePhoto />,
 };
 
 const BRANDON :TeamMemberType = {
   NAME: 'Brandon Wright',
-  ROLE: 'Product Management Lead',
+  ROLE: 'Product Manager',
   PHOTO: <BrandonPhoto />,
 };
 
